0687267eb2a4960fe0be3f6b7759e2c93ad9f9ce
gauravhadge-29 <gauravhadge.official@gmail.com>
2025-02-22 16:37:52 +0530

    /

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ app.use(
     })
 );
 app.use(cors({
-  origin: "https://login-eta-lemon.vercel.app/",
+  origin: "https://login-eta-lemon.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
