c9335039e146c907b6bfb52266d313b75b705e7b
gauravhadge-29 <gauravhadge.official@gmail.com>
2025-02-22 17:15:54 +0530

    cheking auth-2

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -9,6 +9,7 @@ import jwt from "jsonwebtoken";
 export const verifyJWT = asyncHandler(async(req,res,next)=>{
    try {
     console.log("request came")
+    console.log(req.cookies)
      const token = req.cookies?.accessToken || req.header
      ("Authorization")?.replace("Bearer", "")
      console.log("Token is",token)
