d456bc65b4aa18970aca253bb2aee938aff81554
gauravhadge-29 <gauravhadge.official@gmail.com>
2025-02-23 02:23:17 +0530

    updated main

--- frontend/login-page/src/main.jsx
+++ frontend/login-page/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
+import ReactDOM from "react-dom/client"; // Use createRoot from React 18+
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
