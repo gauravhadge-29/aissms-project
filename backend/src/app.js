b4f8e449ee9e9ed45fcc12fa9b735b3de8487cfa
gauravhadge-29 <gauravhadge.official@gmail.com>
2025-02-22 15:41:38 +0530

    minor change

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,7 +24,7 @@ app.use(
     })
 );
 app.use(cors({
-  origin: "https://login-teal-psi.vercel.app/",
+  origin: "https://login-teal-psi.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
